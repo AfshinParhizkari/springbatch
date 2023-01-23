4dedd03587d16e9acbde5397ae37366db23fd12d
afshinparhizkari <Afshin.Parhizkari@gmail.com>
2023-01-23 16:51:42 +0330

    add docker compose

--- csv2dbtask/src/main/resources/mysql-ddl-person.sql
+++ csv2dbtask/src/main/resources/mysql-ddl-person.sql
@@ -1,4 +1,8 @@
-CREATE TABLE `people` (
- `FIRST_NAME` varchar(50) NOT NULL,
- `LAST_NAME` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CH
+CREATE TABLE test_general.people (
+ FIRST_NAME varchar(50) NOT NULL,
+ LAST_NAME varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_persian_ci
+COMMENT='';
+
+SELECT * FROM test_general.people;
