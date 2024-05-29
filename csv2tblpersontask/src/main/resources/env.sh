ad26e902e2e957a9b3667617878e5dfacc85ff8c
afshin.parhizkari@gmail.com <afshin.parhizkari@gmail.com>
2024-05-29 13:45:57 +0330

    prepare version for scdf server

---
+++ csv2tblpersontask/src/main/resources/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# SCDF_DRIVER_CLASSNAME=com.mysql.cj.jdbc.Driver;SCDF_CONNECTION_STRING=jdbc:mysql://afshin:3306/dataflow; SCDF_USER_NAME=admin; SCDF_PASSWORD=123456; BIZ_DRIVER_CLASSNAME=com.mysql.cj.jdbc.Driver; BIZ_CONNECTION_STRING=jdbc:mysql://afshin:3306/test_general; BIZ_USER_NAME=admin; BIZ_PASSWORD=123456
+export SCDF_DRIVER_CLASSNAME=com.mysql.cj.jdbc.Driver
+export SCDF_CONNECTION_STRING=jdbc:mysql://afshin:3306/dataflow
+export SCDF_USER_NAME=admin
+export SCDF_PASSWORD=123456
+
+export BIZ_DRIVER_CLASSNAME=com.mysql.cj.jdbc.Driver
+export BIZ_CONNECTION_STRING=jdbc:mysql://afshin:3306/test_general
+export BIZ_USER_NAME=admin
+export BIZ_PASSWORD=123456
